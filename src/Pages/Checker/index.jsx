d795af6db7c271f4b5aa165482187ebba56323cc
Yury Regis <yury_regis@hotmail.com>
2021-12-08 03:55:21 -0300

    implementing feature

--- src/Pages/Checker/index.jsx
+++ src/Pages/Checker/index.jsx
@@ -1,26 +1,80 @@
 import * as Styled from './style'
 import React, {useEffect, useState} from 'react'
 import gif from '../../assets/images/loading.gif'
+import gifError from '../../assets/images/error.gif'
 import Button from '../../Components/Button'
 import Input from '../../Components/Input'
+import ResetListeners from '../../utils/resetListeners'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 
+const {ipcRenderer} = window.require('electron')
+
 function Generator() {
+    const [isError, setIsError] = useState(false)
     const [loading, setLoading] = useState(false)
+    const [lastUpdate, setLastUpdate] = useState('.')
     const [hashInput, setHashInput] = useState('')
     const [filePath, setFilePath] = useState('')
+    const [hash, setHash] = useState('')
+    const [result, setResult] = useState('')
+
 
     function handleGenerate() {
-        if(filePath==='' || hashInput==='')
-            return toast.error('Opa! Existe algum campo vazio?') 
-        setLoading(loading => !loading)
+        if(filePath==='' || hashInput==='') {
+            setIsError(_ => true)
+            return toast.error('Opa! Existe algum campo vazio?')
+        }
+        if (filePath === lastUpdate) 
+            return 
+        setLoading(_ => true)
+        ipcRenderer.send('generate-hash', filePath)
     }
 
     function handleGetFile(){
-        
+        ipcRenderer.send('open-file-dialog')
     }
+
+    function checkHashFile() {
+        const check = hash === hashInput
+        if (check)
+            return 'HASH VÁLIDA'
+        else 
+            return 'OPA! HASH INVÁLIDA'
+    }
+
+    useEffect(() => {
+        ResetListeners()
+        ipcRenderer.on('encrypted-hash', (_, arg) => setHash(_ => arg))
+
+        ipcRenderer.on('selected-file', (_, path) => setFilePath(_ => path))
+
+        ipcRenderer.on('invalid-path', (_, message) => {
+            setLoading(_ => false)
+            setIsError(_ => true)
+            toast.error(message)
+        })
+    },[])
+
+    useEffect(() => {
+        if(isError)
+            setTimeout(()=>{
+                setIsError(_ => false)
+            },5000)      
+    }, [isError])
+
+    useEffect(() => {
+        setLastUpdate(filePath)
+        setResult(_ => checkHashFile())
+        setTimeout(()=>{
+            setLoading(_ => false)
+        }, 2000)
+    }, [hash])
+
+    useEffect(() => {
+        setResult(_ => '')
+    },[hashInput])
 
     return (
         <Styled.Container>
@@ -58,10 +112,9 @@ function Generator() {
             <Styled.Canvas>
                 { loading 
                 ? <Styled.LoadingImage src={gif}/>
-                : <>
-                    <Styled.Text>RESULTADO:</Styled.Text>
-                    <Styled.Text size={'12px'}>2e966c4fffeab1ac7126acbb2413cfa315d9915cb0bb2959a2145dfa5511cef4</Styled.Text>
-                  </> }
+                : isError 
+                ? <Styled.LoadingImage src={gifError}/>
+                : <Styled.Title alternative>{result}</Styled.Title>}
             </Styled.Canvas>
             
             <ToastContainer 
