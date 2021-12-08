6802601a88fb35a7183ce5c32589c862d7918f41
Yury Regis <yury_regis@hotmail.com>
2021-12-08 03:34:02 -0300

    Fix openDialog error

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const OS = require('os')
+const fs = require('fs')
+const crypto = require('crypto')
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 
-//require('@electron/remote/main').initialize()
-
+process.setMaxListeners(20);
 
 function createWindow() {
     const appWindow = new BrowserWindow({
@@ -20,19 +21,33 @@ function createWindow() {
 
     appWindow.loadURL('http://localhost:3000')
 
-    //ipcMain.on('open-file-dialog', (event) => {
-    //    const isMacOs = OS.platform() === 'darwin'
-    //
-    //    dialog.showOpenDialog({
-    //        properties: isMacOs 
-    //            ? ['openFile', 'openDirectory']
-    //            : ['openFile']
-    //    }, (files) => {
-    //        if(files) {
-    //            event.sender.send('selected-file', files[0])
-    //        }
-    //    })
-    //})
+    ipcMain.on('open-file-dialog', (event) => {
+        const isMacOs = OS.platform() === 'darwin'
+    
+        const file = dialog.showOpenDialogSync({
+            properties: isMacOs 
+                ? ['openFile', 'openDirectory']
+                : ['openFile']
+            }, (files) => {
+                if(files) 
+                    return event.sender.send('selected-file', files[0])
+        })
+        if(!file) return
+        file[0] && event.sender.send('selected-file', file[0])
+    })
+
+    ipcMain.on('generate-hash', (event, arg) => {
+        try {
+            const file = fs.readFileSync(arg)
+            const sha256 = crypto.createHash('sha256')
+            const encryptFile = sha256.update(file).digest('hex')
+
+            event.sender.send('encrypted-hash', encryptFile)
+        } catch (error) {
+            const message = 'Arquivo ou diretório inválido!'
+            event.sender.send('invalid-path', message)
+        }
+    })
 }
 
 ipcMain.on('close-main-window', () => app.quit())
