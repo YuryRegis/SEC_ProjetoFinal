79272384e38121b1ea07d016a660bc19d20f91fd
Yury Regis <yury_regis@hotmail.com>
2021-12-11 22:02:07 -0300

    Change divider from dash to solid

--- src/Components/DashLine/style.js
+++ src/Components/DashLine/style.js
@@ -7,6 +7,6 @@ export const SpaceDashLine = styled.div`
     margin: ${({size}) => size ? size : 30}px 0 0 0;
 
     border-color: #666666;
-    border-style: dashed;
-    border-width: 0 0 5px 0;
+    border-style: solid;
+    border-width: 0 0 2px 0;
 `
